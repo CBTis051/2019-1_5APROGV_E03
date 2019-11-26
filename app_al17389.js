c2bfead40906b2d923d444f50ea5104aef7f29d8
Daniel Armando Cortes Navarro <al17389@cbtis051.edu.mx>
2019-11-26 14:01:40 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17389.js
+++ app_al17389.js
@@ -2,6 +2,13 @@ var http = require('http');
 http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'application/json; charset=utf8'});
 
-        res.end('Hola mundo node JS Repo GIT - Cortes Navarro Daniel Armando\n');
+        var objeto = {
+                mensaje : 'Hola mundo node JS Repo GIT - Cortes Navarro Daniel Armando\n'
+        };
+
+        var json = JSON.stringify(objeto);
+
+        res.end(json);
+
 }).listen(8081);
 console.log('Server running on part 8081');
