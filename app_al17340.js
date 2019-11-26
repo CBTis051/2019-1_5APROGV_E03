695d1d81e020240c450f7d233732a7992ec77f14
Pereida morones jesus manuel <al17363@cbtis051.edu.mx>
2019-11-26 15:23:04 -0700

    Ajustes en la respuesta a formato JSON

---
+++ app_al17340.js
@@ -0,0 +1,13 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
+    
+    var objeto = {
+        mensaje: 'Hola mundo Node JS Repo GIT - Camberos Hipolito Angel\n'
+    };
+
+    var json=JSON.stringify(objeto);
+
+    res.end(JSON);
+}).listen(8081);
+console.log('Server running on port 8081');
