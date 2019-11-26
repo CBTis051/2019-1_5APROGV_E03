b0d50a886568842b52897502f8311d8bccced7e7
Pereida morones jesus manuel <al17363@cbtis051.edu.mx>
2019-11-26 14:30:23 -0700

    ajustes en la respuesta a formato JSON

--- app_al17363.js
+++ app_al17363.js
@@ -1,6 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
-        res.writeHead(200, {'Content-Type': 'application/json; charset=utf8'});
+
+        res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
 
         var odjeto = {
         mensaje :'Hola mundo node JS Repo GIT - Pereida Morones Jesus manuel\n'
@@ -9,6 +10,6 @@ http.createServer(function (req, res) {
         var json = JSON.stringify(objecto);
 
         res.end (json);
-        
+
 }).listen(8081);
 console.log('Server running on part 8081');
