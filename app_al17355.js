42d55e6aa719157aa029d448868a7f81c8272064
Luis Eduardo Medina Mayorquin <al17355@cbtis051.edu.mx>
2019-11-26 15:10:01 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17355.js
+++ app_al17355.js
@@ -1,7 +1,14 @@
 var http = require('http');
 http.createServer(function (req, res) {
+
     res.writeHead(200, {'content-Type': 'application/json; charset=utf-8'});
 
-    res.end('Hola mundo Node JS Repo GIT - Medina Mayorquin Luis Eduardo\n');
+    var objeto = {
+        mensaje : "Hola mundo Node JS Repo GIT - Medina Mayorquin Luis eduardo\n"
+    };
+
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
 }).listen(8081);
 console.log('Server running on port 8081');
